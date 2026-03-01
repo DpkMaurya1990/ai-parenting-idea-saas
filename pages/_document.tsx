f8f8e1d734187a1e361ad72b809ffaa314d6f87b
DpkMaurya1990 <deepak.mau2@gmail.com>
2026-03-01 22:37:01 +0530

    Initial commit - AI Parenting Idea SaaS

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <title>Parenting Idea Support</title>
+        <meta name="description" content="AI-powered parenting idea support" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
